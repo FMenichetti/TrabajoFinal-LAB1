67abb021c2c1f99cb4ba4369e07d19ddd27a3df9
FMenichetti <129986699+FMenichetti@users.noreply.github.com>
2024-05-30 19:22:43 -0300

    clase entrenador--

--- ProyectoFinal_LAB1/src/Entidades/Entrenador.java
+++ ProyectoFinal_LAB1/src/Entidades/Entrenador.java
@@ -7,75 +7,29 @@ public class Entrenador {
     private String nombre;
     private String apellido ;
     private String especialidad;
-    private String disponibilidad;
+    private boolean estado;
 
     public Entrenador() {
     }
 
-    public Entrenador(String dni, String nombre, String apellido, String especialidad, String disponibilidad) {
+    public Entrenador(String dni, String nombre, String apellido, String especialidad, boolean estado) {
         this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.especialidad = especialidad;
-        this.disponibilidad = disponibilidad;
+        this.estado = estado;
     }
 
-    public Entrenador(int idEntrenador, String dni, String nombre, String apellido, String especialidad, String disponibilidad) {
+    public Entrenador(int idEntrenador, String dni, String nombre, String apellido, String especialidad, boolean estado) {
         this.idEntrenador = idEntrenador;
         this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.especialidad = especialidad;
-        this.disponibilidad = disponibilidad;
+        this.estado = estado;
     }
 
-    public int getIdEntrenador() {
-        return idEntrenador;
-    }
-
-    public void setIdEntrenador(int idEntrenador) {
-        this.idEntrenador = idEntrenador;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
-
-    public String getDisponibilidad() {
-        return disponibilidad;
-    }
-
-    public void setDisponibilidad(String disponibilidad) {
-        this.disponibilidad = disponibilidad;
-    }
+    
 
     @Override
     public String toString() {
