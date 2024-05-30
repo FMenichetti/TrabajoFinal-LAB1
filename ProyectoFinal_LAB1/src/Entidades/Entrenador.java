f089628372d6fd169b736a83034760a8b7818590
FMenichetti <129986699+FMenichetti@users.noreply.github.com>
2024-05-30 20:22:28 -0300

    entrenador para push

--- ProyectoFinal_LAB1/src/Entidades/Entrenador.java
+++ ProyectoFinal_LAB1/src/Entidades/Entrenador.java
@@ -29,6 +29,54 @@ public class Entrenador {
         this.estado = estado;
     }
 
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
+
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
     
 
     @Override
