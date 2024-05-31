cc34d1feb0b4d8adaea12f10127a784f0386a3b0
Edu Lopez Code <edulopez.dg@gmail.com>
2024-05-30 21:56:15 -0300

    Entidad AccesoMembresia y Membresia
    Se crean y completan ambas clases, restan definir detalles pero están completas

--- ProyectoFinal_LAB1/src/AccesoDatos/AccesoMembresia.java
+++ ProyectoFinal_LAB1/src/AccesoDatos/AccesoMembresia.java
@@ -1,5 +1,174 @@
 package AccesoDatos;
 
+import Entidades.Membresia;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 public class AccesoMembresia {
 
+    private Connection con;
+
+    public AccesoMembresia() {
+        con = Conexion.getConexion();
+    }
+
+    public void crearMembresia(Membresia membresia) {
+        String sql = "INSERT INTO `membresias`(`idSocio`, `tipo`, `fechaInicio`, `fechaFin`, `estado`) VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            //ps.setInt(1, membresia.getIdSocio());
+            ps.setString(2, membresia.getTipo());
+            ps.setDate(3, Date.valueOf(membresia.getFechaInicio()));
+            ps.setDate(4, Date.valueOf(membresia.getFechaFin()));
+            ps.setBoolean(5, membresia.isEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Membresía añadida");
+            }
+            ps.close();
+        } catch (SQLIntegrityConstraintViolationException x) {
+            JOptionPane.showMessageDialog(null, "El socio ya tiene una membresía activa");
+        } catch (SQLException S) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresias: " + S);
+        } catch (Exception e) {
+            System.out.println("Error general: " + e.getMessage());
+        }
+    }
+
+    public Membresia buscarMembresiaPorIdSocio(int idSocio) {
+        Membresia membresia = null;
+        String sql = "SELECT `idMembresia`, `tipo`, `fechaInicio`, `fechaFin`, `estado` FROM `membresias` WHERE idSocio=? AND estado = 1";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idSocio);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                membresia = new Membresia();
+                membresia.setIdMembresia(rs.getInt("idMembresia"));
+                //membresia.setIdSocio(idSocio);
+                membresia.setTipo(rs.getString("tipo"));
+                membresia.setFechaInicio(rs.getDate("fechaInicio").toLocalDate());
+                membresia.setFechaFin(rs.getDate("fechaFin").toLocalDate());
+                membresia.setEstado(rs.getInt("estado") == 1);
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe la membresía para el socio con ID: " + idSocio);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresias: " + e.getMessage());
+        }
+        return membresia;
+    }
+
+    public Membresia buscarMembresiaPorDniSocio(String dniSocio) {
+        Membresia membresia = null;
+        String sqlIdSocio = "SELECT `idSocio` FROM `socios` WHERE dni = ?";
+        try {
+            PreparedStatement psIdSocio = con.prepareStatement(sqlIdSocio);
+            psIdSocio.setString(1, dniSocio);
+
+            ResultSet rsIdSocio = psIdSocio.executeQuery();
+            if (rsIdSocio.next()) {
+                int idSocio = rsIdSocio.getInt("idSocio");
+
+                String sqlMembresia = "SELECT `idMembresia`, `tipo`, `fechaInicio`, `fechaFin`, `estado` FROM `membresias` WHERE idSocio=? AND estado = 1";
+                PreparedStatement psMembresia = con.prepareStatement(sqlMembresia);
+                psMembresia.setInt(1, idSocio);
+
+                ResultSet rsMembresia = psMembresia.executeQuery();
+                if (rsMembresia.next()) {
+                    membresia = new Membresia();
+                    membresia.setIdMembresia(rsMembresia.getInt("idMembresia"));
+                   // membresia.setIdSocio(idSocio);
+                    membresia.setTipo(rsMembresia.getString("tipo"));
+                    membresia.setFechaInicio(rsMembresia.getDate("fechaInicio").toLocalDate());
+                    membresia.setFechaFin(rsMembresia.getDate("fechaFin").toLocalDate());
+                    membresia.setEstado(rsMembresia.getInt("estado") == 1);
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se encontró la membresía para el socio con DNI: " + dniSocio);
+                }
+                psMembresia.close();
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontró un socio con DNI: " + dniSocio);
+            }
+            psIdSocio.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla socios o membresias: " + e.getMessage());
+        }
+        return membresia;
+    }
+
+    public List<Membresia> listarMembresia() {
+        List<Membresia> membresias = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM membresias WHERE estado = 1";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Membresia membresia = new Membresia();
+                membresia.setIdMembresia(rs.getInt("idMembresia"));
+               // membresia.setIdSocio(rs.getInt("idSocio"));
+                membresia.setTipo(rs.getString("tipo"));
+                membresia.setFechaInicio(rs.getDate("fechaInicio").toLocalDate());
+                membresia.setFechaFin(rs.getDate("fechaFin").toLocalDate());
+                membresia.setEstado(rs.getInt("estado") == 1);
+                membresias.add(membresia);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresias: " + e.getMessage());
+        }
+        return membresias;
+    }
+
+    public void modificarMembresia(Membresia membresia) {
+        String sql = "UPDATE membresias SET idSocio = ?, tipo = ?, fechaInicio = ?, fechaFin = ? WHERE idMembresia = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            //ps.setInt(1, membresia.getIdSocio());
+            ps.setString(2, membresia.getTipo());
+            ps.setDate(3, Date.valueOf(membresia.getFechaInicio()));
+            ps.setDate(4, Date.valueOf(membresia.getFechaFin()));
+            ps.setInt(5, membresia.getIdMembresia());
+
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Membresía modificada");
+            } else {
+                JOptionPane.showMessageDialog(null, "La membresía no existe");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al modificar la membresía: " + e.getMessage());
+        }
+    }
+
+    public void eliminarMembresia(int id) {
+        try {
+            String sql = "UPDATE membresias SET estado = 0 WHERE idMembresia = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            int fila = ps.executeUpdate();
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, "Se eliminó la membresía");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontró la membresía con el ID proporcionado");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresias: " + e.getMessage());
+        }
+    }
 }
--- ProyectoFinal_LAB1/src/Entidades/Membresia.java
+++ ProyectoFinal_LAB1/src/Entidades/Membresia.java
@@ -1,5 +1,85 @@
 package Entidades;
 
-public class Membresia {
+import java.time.LocalDate;
 
+public class Membresia {
+    //Atributos
+    private int idMembresia;
+    private Socio idSocio;
+    private String tipo;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private boolean estado;
+    
+    //Constructor vacío
+    public Membresia() {
+    }
+    
+    //Constructor sin idMembresia
+    public Membresia(Socio idSocio, String tipo, LocalDate fechaInicio, LocalDate fechaFin, boolean estado) {
+        this.idSocio = idSocio;
+        this.tipo = tipo;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+    }
+    
+    //Constructor completo
+    public Membresia(int idMembresia, Socio idSocio, String tipo, LocalDate fechaInicio, LocalDate fechaFin, boolean estado) {
+        this.idMembresia = idMembresia;
+        this.idSocio = idSocio;
+        this.tipo = tipo;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+    }
+    
+    //Metodos Getters & Setters
+    public int getIdMembresia() {
+        return idMembresia;
+    }
+    public void setIdMembresia(int idMembresia) {
+        this.idMembresia = idMembresia;
+    }
+    public Socio getIdSocio() {
+        return idSocio;
+    }
+    public void setIdSocio(Socio idSocio) {
+        this.idSocio = idSocio;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    public boolean isEstado() {
+        return estado;
+    }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    //Metodo toString()
+    @Override
+    public String toString() {
+        return "Membresia{" + "idMembresia=" + idMembresia + ", idSocio=" 
+                + idSocio + ", tipo=" + tipo + ", fechaInicio=" + fechaInicio 
+                + ", fechaFin=" + fechaFin + ", estado=" + estado + '}';
+    }
+    
+       
+    
 }
